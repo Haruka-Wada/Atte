ff2908f7b52f42348d55d57884044bdb96ef9d24
haruka wada <022.wadaha@gmail.com>
2024-09-08 17:01:01 +0900

    restテーブル作成、休憩開始と終了の打刻機能追加

---
+++ .history/src/app/Models/Rest_20240908154028.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['work_id', 'start_time', 'end_time'];
+
+    protected $guarded = ['id'];
+
+    public function work() {
+        return $this->belongsTo('App\Models\Work')
+    }
+}
