82a1ef85fb37f2a7ce6824a9bd8554296b3d3ee1
haruka wada <022.wadaha@gmail.com>
2024-09-13 21:00:51 +0900

    日付毎表示wo修正

---
+++ .history/src/app/Http/Controllers/AtteController_20240913153236.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Work;
+use App\Models\Rest;
+use Carbon\Carbon;
+
+class AtteController extends Controller
+{
+    public function index() {
+        $user = Auth::user();
+        return view ('stamp', compact('user'));
+    }
+
+    public function attendance() {
+        $dt = Carbon::now()->format('Y-m-d');
+        $works = Work::with('user')->where('start_time', 'LIKE', $dt.'%')->paginate(5);
+        return view('attendance', compact('dt', 'works'));
+    }
+
+    public function changeDate(Request $request) {
+        dd($request->d)
+        $dt = Carbon::createFromFormat('Y-m-d', '$request->dt');
+        if($request->sub == 'sub') {
+            $dt = $dt->subDay();
+        }elseif($request->add == 'add') {
+            $dt = $dt->addDay();
+        }
+        $works = Work::with('user')->where('start_time', 'LIKE', $dt.'%')->paginate(5);
+        return view('attendance', compact('dt', 'works'));
+    }
+
+    public function workStart() {
+        $work = Work::create([
+            'user_id' => Auth::id(),
+            'start_time' => Carbon::now(),
+        ]);
+
+        return redirect('/');
+    }
+
+    public function workEnd() {
+        $work = Work::where('user_id', Auth::id())->latest()->first();
+        $work->update([
+            'end_time' => Carbon::now()
+        ]);
+
+        return redirect('/');
+    }
+
+    public function restStart() {
+        $work = Work::where('user_id', Auth::id())->latest()->first();
+        $rest = Rest::create([
+            'work_id' => $work->id,
+            'start_time' => Carbon::now()
+        ]);
+
+        return redirect('/');
+    }
+
+    public function restEnd() {
+        $work = Work::where('user_id', Auth::id())->latest()->first();
+        $rest = Rest::where('work_id', $work->id);
+        $rest->update([
+            'end_time' => Carbon::now()
+        ]);
+
+        return redirect('/');
+    }
+}
