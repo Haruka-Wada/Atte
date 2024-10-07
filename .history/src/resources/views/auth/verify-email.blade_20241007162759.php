2c9202d27e533698f83c9b4cb948ef902e852d36
haruka wada <022.wadaha@gmail.com>
2024-10-07 17:33:13 +0900

    メール認証機能実装

---
+++ .history/src/resources/views/auth/verify-email.blade_20241007162759.php
@@ -0,0 +1,38 @@
+@extends('layouts/app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('main')
+<div class="mb-4 text-sm text-gray-600">
+    ご登録ありがとうございます！<br>
+    ご入力いただいたメールアドレスへ認証リンクを送信しましたので、クリックして認証を完了させてください。<br>
+    もし、認証メールが届かない場合は再送させていただきます。
+</div>
+
+@if (session('status') == 'verification-link-sent')
+<div class="mb-4 font-medium text-sm text-green-600">
+    新しい認証メールが送信されました。
+</div>
+@endif
+
+<div class="mt-4 flex items-center justify-between">
+    <form method="POST" action="/email/verification-notification">
+        @csrf
+
+        <div>
+            <button type="submit">
+                認証メールを再送する
+            </button>
+        </div>
+    </form>
+
+    <form method="POST" action="/logout">
+        @csrf
+        <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+            ログアウト
+        </button>
+    </form>
+</div>
+@endsection
