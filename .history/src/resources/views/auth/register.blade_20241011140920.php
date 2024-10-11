5c0a9614b559216b1792f71834e3ea05bdc3e6b0
haruka wada <022.wadaha@gmail.com>
2024-10-11 15:01:18 +0900

    register画面のバリデーション追加、view画面の編集

---
+++ .history/src/resources/views/auth/register.blade_20241011140920.php
@@ -0,0 +1,47 @@
+@extends('layouts/app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
+@endsection
+
+@section('main')
+<div class="main-container">
+    <div class="main-title">
+        <p class="main-title__text">会員登録</p>
+    </div>
+    <div class="main-form">
+        <form action="/register" method="post">
+            @csrf
+            <input type="text" class="main-form__input" name="name" placeholder="名前" value="{{ old('name') }}">
+            <span class="error-message">
+                @error('name')
+                {{ $message }}
+                @enderror
+            </span>
+            <input type="email" class="main-form__input" name="email" placeholder="メールアドレス" value="{{ old('email') }}">
+            <span class="error-message">
+                @error('email')
+                {{ $message }}
+                @enderror
+            </span>
+            <input type="password" class="main-form__input" name="password" placeholder="パスワード">
+            <span class="error-message">
+                @error('password')
+                {{ $message }}
+                @enderror
+            </span>
+            <input type="password" class="main-form__input" name="password_confirmation" placeholder="確認用パスワード">
+            <span class="error-message">
+                @error('')
+                {{ $message }}
+                @enderror
+            </span>
+            <button class="main-form__button">会員登録</button>
+        </form>
+    </div>
+    <div class="main-login">
+        <p>アカウントをお持ちの方はこちらから</p>
+        <a href="/login">ログイン</a>
+    </div>
+</div>
+@endsection
